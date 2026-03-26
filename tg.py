12171d75c0d94690b636529d46b243fee3948f4d
Daniktop <73233974+Daniktop@users.noreply.github.com>
2026-03-26 09:10:49 +0300

    Add files via upload

--- tg.py
+++ tg.py
@@ -1,17 +1,13 @@
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = "8498298692:AAEnYgsoJ4MhkSbsjdZiR5fFLXycz5J2l7o"
+TOKEN = os.getenv("8498298692:AAEnYgsoJ4MhkSbsjdZiR5fFLXycz5J2l7o")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я тестовый бот 🤖")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я работаю на Railway 🚂")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
 app.run_polling()
